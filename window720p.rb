7c9c452d92e2f9da86d28030c8a46d5c36629d9e
Masaki Komagata <komagata@gmail.com>
2015-01-14 17:39:25 +0900

    Fix sha256

--- window720p.rb
+++ window720p.rb
@@ -1,7 +1,7 @@
 class Window720p < GithubGistFormula
   homepage "https://gist.github.com/komagata/c6049db24af1af8ace32"
   url "https://gist.githubusercontent.com/komagata/c6049db24af1af8ace32/raw/57a2f72030f147797c8904f57cf73f37807bf9ea/window720p"
-  sha256 "2610b5155263c159026707f35dd564c33e790336c3d25c7278881d809ed70580"
+  sha256 "d2b74c875920a0b201a32ee8962f69b3de72243813e88f36b8603aee1edbcfc5"
   version "0.0.1"
 
   def install
