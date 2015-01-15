670530be2260baae1cb4b2b0379fff1675c42dd4
Masaki Komagata <komagata@gmail.com>
2015-01-15 18:29:29 +0900

    Fix hash

--- csv2yml.rb
+++ csv2yml.rb
@@ -1,8 +1,8 @@
 class Csv2yml < GithubGistFormula
   homepage "https://gist.github.com/komagata/2919d26f4259775fa3a1"
   url "https://gist.githubusercontent.com/komagata/2919d26f4259775fa3a1/raw/35190fe70ea57e2f214dd8b1b882df6bf2f17057/csv2yml.py"
-  sha256 "d2b74c875920a0b201a32ee8962f69b3de72243813e88f36b8603aee1edbcfc5"
-  version "0.0.1"
+  sha256 "8330f817b1ecdf2662d63774aab837f82650554683a2f8c1249070b68cd9247e"
+  version "0.0.2"
 
   def install
     bin.install Dir["*"]
